3342d5db6b4513f2f18cfb818936731ce789ea6c
Gus Viola <gus.viola@pobox.com>
2018-04-03 08:49:39 -0300

    Add paint function
    single click paints red, double click paints white
    cleared some comments
    added a newer jquery script tag, with integrity hash
    small adjustments at css

--- designs.js
+++ designs.js
@@ -1,64 +1,46 @@
-
 //$(document).ready(() => {
-// Select color input
-// Select size input
+
+// Link gridRows and gridColumns to sizePicker
+// Link colorInput to colorPicker
+// Implement Function clearGrid
 
 // When size is submitted by the user, call makeGrid()
-/*
-var grid = makeGrid(10,10);
+let gridRows, gridColumns, colorInput;
 
-$('#mainButton').click($('#pixelCanvas').appendChild(grid););
+//gridRows = $('#inputHeight').val();  //does  not work as it should
+gridRows = 5;
+gridColumns = 5;
+colorInput = 'red';
 
+$('#sizePicker').submit(  function() {  //unstable
+    makeGrid(gridRows, gridColumns);
 
-$( "#mainButton" ).click(function() {
-  alert( "Handler for .click() called." );
 });
 
 
-$("#mainButton").click(makeGrid(10,10)); */
+$('#mainButton').on('click', () => {
+  makeGrid(gridRows, gridColumns);
+});
 
+//Single click paints TD with colorInput
+$('body').on('click', 'td', function() {
+  $(this).css('background-color', colorInput);
+});
 
-
-$("#mainButton").on('click', () => {
-  //$('#colorPicker').toggle();
-  makeGrid(5,5);
-}); 
+//Double Click paints TD white
+$('body').on('dblclick','td', function() {
+        $(this).css('background-color', 'white');
+    });
 
 
 function makeGrid(numRows, numColumns) {
-  /*
-  Function adapted from https://stackoverflow.com/questions/9140101/creating-a-clickable-grid-in-a-web-browser
-  and from http://slavchoslavchev.com/tutorials/building-a-grid-matrix-in-javascript/
-  and from https://codepen.io/nakessler/pen/qOdJWm
-  */
-
 
   const grid = $('#pixelCanvas');
 
   for (let rows = 0; rows < numRows; rows++) {
     grid.append(`<tr class="row ${rows}"></tr>`);
-            for (var columns = 0; columns < numColumns; columns++) {
-            $("tr:last-child").append(`<td></td>`);
-          };
-      };
-  }
-/*
-  for (let r=0;r<numRows;r++){
-    tr = grid.append(`<tr></tr>`);
-    for (let c=0;c<numColumns;c++){
-
-
-
-
-      cell.addEventListener('click',(function(el,r,c,i){
-        return function(){ callback(el,r,c,i); }
-       })(cell,r,c,i),false);*/
-  //  }
-  //}
-
-
-  //return grid;
-  //alert( "function MakeGrid at the end." );
-//}
-
-//}
+    for (var columns = 0; columns < numColumns; columns++) {
+      $("tr:last-child").append(`<td></td>`);
+    };
+  };
+}
