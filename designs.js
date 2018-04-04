74778e30042c93d40b0e88eaa7a2682a9212fb70
Gus Viola <gus.viola@pobox.com>
2018-04-04 08:26:18 -0300

    Link grid size and color to user input
    +Javascript:
    Link gridRows and gridColumns to sizePicker
    Link colorValue to colorPicker
    Clear grid before drawing
    Adjust variable names ($camelCase)
    Eliminate main button and adjust submit form (event.preventDefault)
    +HTML:
    meta viewport
    Adjust form settings using full extent of HTML 5
    Comment out main button
    +CSS:
    Put colors where they belong, body not td

--- designs.js
+++ designs.js
@@ -1,44 +1,51 @@
-//$(document).ready(() => {
+//$(document).load(() => {
 
-// Link gridRows and gridColumns to sizePicker
-// Link colorInput to colorPicker
-// Implement Function clearGrid
+// Implement https://github.com/okcoker/jquery-airport
+//Improve fonts
+
 
 // When size is submitted by the user, call makeGrid()
-let gridRows, gridColumns, colorInput;
+let $gridRows, $gridColumns, $colorValue, $colorBackground;
+const $grid = $('#pixelCanvas');
 
-//gridRows = $('#inputHeight').val();  //does  not work as it should
-gridRows = 5;
-gridColumns = 5;
-colorInput = 'red';
+//$colorValue = 'red';
+$colorBackground = $grid.css('background-color');
 
-$('#sizePicker').submit(  function() {  //unstable
-    makeGrid(gridRows, gridColumns);
-
+$('#sizePicker').submit(  function(event) {  //unstable
+    //the submit button is for form admission, thus we need the line below
+    //to avoid referencing to an unexisting admission website
+    event.preventDefault();
+    $gridRows = $('#inputHeight').val();
+    $gridColumns = $('#inputWeight').val();
+    makeGrid($gridRows, $gridColumns);
 });
 
 
-$('#mainButton').on('click', () => {
+/*$('#mainButton').on('click', () => {
   makeGrid(gridRows, gridColumns);
-});
+});*/
 
 //Single click paints TD with colorInput
 $('body').on('click', 'td', function() {
-  $(this).css('background-color', colorInput);
+  $colorValue = $('#colorPicker').val();
+  $(this).css('background-color', $colorValue);
 });
 
 //Double Click paints TD white
 $('body').on('dblclick','td', function() {
-        $(this).css('background-color', 'white');
+        $(this).css('background-color', $colorBackground);
     });
 
+//}// close document-ready tag
 
 function makeGrid(numRows, numColumns) {
 
-  const grid = $('#pixelCanvas');
+  //const $grid = $('#pixelCanvas');
+
+  $grid.empty();      //clears the grid
 
   for (let rows = 0; rows < numRows; rows++) {
-    grid.append(`<tr class="row ${rows}"></tr>`);
+    $grid.append(`<tr class="row ${rows}"></tr>`);
     for (var columns = 0; columns < numColumns; columns++) {
       $("tr:last-child").append(`<td></td>`);
     };
