52bb2ff895c0d1a28b8e000dcd7ba991ae00e83d
Gus Viola <gus.viola@pobox.com>
2018-04-05 08:40:51 -0300

    Adjust javascript removing comments

--- js/designs.js
+++ js/designs.js
@@ -1,14 +1,6 @@
-//$(document).load(() => {
-
-// Implement https://github.com/okcoker/jquery-airport
-//Improve fonts
-
-
-// When size is submitted by the user, call makeGrid()
 let $gridRows, $gridColumns, $colorValue, $colorBackground;
 const $grid = $('#pixelCanvas');
 
-//$colorValue = 'red';
 $colorBackground = $grid.css('background-color');
 
 $('#sizePicker').submit(  function(event) {  //unstable
@@ -20,12 +12,6 @@ $('#sizePicker').submit(  function(event) {  //unstable
     makeGrid($gridRows, $gridColumns);
 });
 
-
-/*$('#mainButton').on('click', () => {
-  makeGrid(gridRows, gridColumns);
-});*/
-
-//Single click paints TD with colorInput
 $('body').on('click', 'td', function() {
   $colorValue = $('#colorPicker').val();
   $(this).css('background-color', $colorValue);
@@ -36,11 +22,7 @@ $('body').on('dblclick','td', function() {
         $(this).css('background-color', $colorBackground);
     });
 
-//}// close document-ready tag
-
 function makeGrid(numRows, numColumns) {
-
-  //const $grid = $('#pixelCanvas');
 
   $grid.empty();      //clears the grid
 
